dcf01215b785d9485e37db5037dac9981498aeae
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-01-31 12:24:14 +0530

    replaceing files

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'weatherdetector.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templete'],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
